21a8739c19650aef74bdd6d104a2c7dff5d4cbd6
debugtheworldbot <istiancz@gmail.com>
2020-08-16 10:17:49 +0800

    try SSR by getServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import {GetServerSideProps, NextPage} from "next";
+import {UAParser} from 'ua-parser-js'
 
-export default function Home() {
-  return (
-      <div>
-        <h1>header</h1>
-        <p>p</p>
-      </div>
-  )
+type Props={
+    browser:{
+        name:string,
+        version:string,
+        major:string
+    }
+}
+const Home:NextPage<Props>=(props)=> {
+    const {browser}=props
+    return (
+        <div>
+            <h1>Your browser is {browser.name}</h1>
+        </div>
+    );
+}
+export default Home
+export const getServerSideProps:GetServerSideProps=async (context)=>{
+    const ua=context.req.headers['user-agent']
+    const result =await new UAParser(ua).getResult()
+    return  {
+        props:{
+            browser:result.browser
+        }
+    }
 }
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -4,22 +4,13 @@ import {getPosts} from "../../lib/posts";
 
 
 const PostsIndex:NextPage<Props>=(props)=>{
-    console.log(props.posts)
     const {posts}=props
-    debugger
-    // const {posts,isLoading,isEmpty}= usePosts()
     return (
         <div>
             <h1>文章列表</h1>
             {posts.map(p=><div key={p.id}>
                 {p.id}
             </div>)}
-            {/*{isLoading? <div>loading...</div>:*/}
-            {/*    isEmpty?<div>nothing here</div>:*/}
-            {/*        posts.map(item=><div key={item.id}>*/}
-            {/*        {item.title}*/}
-            {/*    </div>*/}
-            {/*)}*/}
     </div>)
 }
 export default PostsIndex
